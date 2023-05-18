16df5bd19779565f2c56c1df35808cda58b11ff6
Claudio F <claudio@monogram.io>
2023-05-18 13:04:15 -0400

    using experimental optimized asset support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
-
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp'
-		})
-	],
+	integrations: [tailwind()],
+	experimental: {
+		assets: true
+	},
+
 	output: 'server',
 	adapter: vercel({
 		analytics: true,
